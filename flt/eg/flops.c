c9fd097f5fd2931dfbb48dca8efa398db3af8ee1
Russell-S-Harper <russell.s.harper@gmail.com>
2024-08-05 18:28:40 -0400

    Revising error from ppm to ppb

--- flt/eg/flops.c
+++ flt/eg/flops.c
@@ -17,30 +17,31 @@ int main() {
 	double a, b, d;
 	clock_t t;
 
-	printf("     FLOPS  ppm\n");
+	printf("           Error\n");
+	printf("     FLOPS (ppb)\n");
 	for (i = 0, a = 10.0, b = 0.01, t = -clock(); i < 1000; ++i)
 		a += b;
 	t += clock();
 	d = (double)t / (double)CLOCKS_PER_SEC;
-	printf("FADD %5.1F %5.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 20.0));
+	printf("FADD %5.1F %4.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 20.0));
 
 	for (i = 0, a = 20.0, b = 0.01, t = -clock(); i < 1000; ++i)
 		a -= b;
 	t += clock();
 	d = (double)t / (double)CLOCKS_PER_SEC;
-	printf("FSUB %5.1F %5.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 10.0));
+	printf("FSUB %5.1F %4.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 10.0));
 
 	for (i = 0, a = 10.0, b = 1.001, t = -clock(); i < 1000; ++i)
 		a *= b;
 	t += clock();
 	d = (double)t / (double)CLOCKS_PER_SEC;
-	printf("FMUL %5.1F %5.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 27.1692393223559));
+	printf("FMUL %5.1F %4.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 27.1692393223559));
 
-	for (i = 0, a = 10.0, b = 0.999, t = -clock(); i < 1000; ++i)
+	for (i = 0, a = 10.0, b = 1.001, t = -clock(); i < 1000; ++i)
 		a /= b;
 	t += clock();
 	d = (double)t / (double)CLOCKS_PER_SEC;
-	printf("FDIV %5.1F %5.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 27.1964221644286));
+	printf("FDIV %5.1F %4.1F\n", 1000.0 / d, 1000000.0 * fabs(1.0 - a / 3.68063304288818));
 	
 	return 0;
 }
